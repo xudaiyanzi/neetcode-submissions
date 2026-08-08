7d0a7ac7353c5372a68c69e4e19cce2a09a22500
Yan Xu <43831818+xudaiyanzi@users.noreply.github.com>
2026-08-08 01:49:25 -0500

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        res = []
+
+        for n in nums:
+            res.append(n)
+        
+        for n in nums:
+            res.append(n)
+        
+        return res
