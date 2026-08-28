046db9a108436a62970f213506f8522a6002f82c
Yan Xu <43831818+xudaiyanzi@users.noreply.github.com>
2026-08-28 13:38:44 -0500

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l, r = 0, 0
+        res = 0
+        letter_dic = {}
+
+        while r < len(s):
+            letter_dic[s[r]] = letter_dic.get(s[r], 0) + 1
+            most_freq = max(letter_dic.values())
+
+            while r - l + 1 - most_freq > k:
+                letter_dic[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+            r += 1
+        
+        return res
+                
